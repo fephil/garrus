c668d20a8e756fb5100b1b43d0879efa3905da86
Phil Lennon <phil@iampjl.co.uk>
2015-10-29 18:32:06 +0000

    gulp load plugins & basic css task

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,23 @@
-// Load modules
+// Load gulp modules
 var gulp        = require('gulp');
+var plugins     = require('gulp-load-plugins')();
+
+// Load non-gulp modules
 var browserSync = require('browser-sync').create();
+var autoprefixer = require('autoprefixer');
 
 // Config
 // TBA
 var buildFolder = '_dist/';
+
+// PostCSS
+gulp.task('css', function () {
+  return gulp.src('./src/*.css')
+    .pipe(plugins.sourcemaps.init())
+    .pipe(plugins.postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(plugins.sourcemaps.write('.'))
+    .pipe(gulp.dest(buildFolder));
+});
 
 // Browsersync
 gulp.task('server', function() {
