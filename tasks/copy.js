9a07e8ffcd5a38c89cdd6e0cb7a9a1e4363a5a5e
Phil Lennon <phil@iampjl.co.uk>
2015-10-31 22:22:43 +0000

    add copy example

---
+++ tasks/copy.js
@@ -0,0 +1,22 @@
+// Load gulp and global config
+var gulp          = require('gulp');
+var config        = require('../config.json');
+var minimist      = require('minimist');
+var gulpif        = require('gulp-if');
+
+// Get command line options we want to use
+var knownOptions = {
+  string: 'env',
+  default: { env: 'dev' }
+};
+
+var options = minimist(process.argv.slice(2), knownOptions);
+
+// Specific task modules
+// ...
+
+// Copy task
+gulp.task('copy', function (callback) {
+    return gulp.src([config.paths.files + '**/*', 'src/test2/**/*'], {})
+    .pipe(gulp.dest(config.paths.build));
+});
