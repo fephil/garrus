fd2bbca38a2141413eec1bb72907fa4c3add0394
Phil Lennon <phil@iampjl.co.uk>
2015-10-31 17:19:51 +0000

    add global config file with folder locations

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
 // Load gulp modules
-var gulp        = require('gulp');
-var plugins     = require('gulp-load-plugins')();
+var gulp          = require('gulp');
+var plugins       = require('gulp-load-plugins')();
 
 // Load non-gulp modules
-var browserSync = require('browser-sync').create();
-var autoprefixer = require('autoprefixer');
+var browserSync   = require('browser-sync').create();
+var autoprefixer  = require('autoprefixer');
 
-// Config
-// TBA
-var buildFolder = '_dist/';
+// Load config
+var config        = require('./config.json');
 
 // PostCSS
 gulp.task('css', function () {
-  return gulp.src('./src/*.css')
+  return gulp.src(config.paths.css + '*.css')
     .pipe(plugins.sourcemaps.init())
-    .pipe(plugins.postcss([ autoprefixer({ browsers: ['last 2 versions'] }) ]))
+    .pipe(plugins.postcss([ autoprefixer({ }) ]))
     .pipe(plugins.sourcemaps.write('.'))
-    .pipe(gulp.dest(buildFolder));
+    .pipe(gulp.dest(config.paths.build));
 });
 
 // Browsersync
 gulp.task('server', function() {
   browserSync.init({
     server: {
-      baseDir: buildFolder
+      baseDir: config.paths.build
     }
   });
 });
