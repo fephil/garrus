32d418ccda8bb2c85b0fc4e602cc645bbd415ed6
Phil Lennon <phil@iampjl.co.uk>
2015-11-01 00:24:06 +0000

    add postcss import, variables, px to rem and normalize

--- tasks/css.js
+++ tasks/css.js
@@ -1,8 +1,8 @@
 // Load gulp and global config
-var gulp          = require('gulp');
-var config        = require('../config.json');
-var minimist      = require('minimist');
-var gulpif        = require('gulp-if');
+var gulp      = require('gulp');
+var config    = require('../config.json');
+var minimist  = require('minimist');
+var gulpif    = require('gulp-if');
 
 // Get command line options we want to use
 var knownOptions = {
@@ -13,15 +13,28 @@ var knownOptions = {
 var options = minimist(process.argv.slice(2), knownOptions);
 
 // Specific task modules
-var sourcemaps    = require('gulp-sourcemaps');
-var postcss       = require('gulp-postcss');
-var autoprefixer  = require('autoprefixer');
+var sourcemaps        = require('gulp-sourcemaps');
+var postcss           = require('gulp-postcss');
+var atImport          = require('postcss-import');
+var customProperties  = require('postcss-custom-properties');
+var autoprefixer      = require('autoprefixer');
+var pxtorem           = require('postcss-pxtorem');
 
-// PostCSS task
+// Postcss processors
+var processors = [
+  atImport(),
+  customProperties(),
+  autoprefixer(),
+  pxtorem({
+    replace: true
+  })
+];
+
+// Postcss task
 gulp.task('css', function () {
   return gulp.src(config.paths.css + '*.css')
     .pipe(gulpif(options.env === 'dev', sourcemaps.init())) // Sourcemaps in dev
-    .pipe(postcss([ autoprefixer({ }) ]))
+    .pipe(postcss(processors))
     .pipe(gulpif(options.env === 'dev', sourcemaps.write('.'))) // Sourcemaps in dev
     .pipe(gulp.dest(config.paths.buildAssets + 'css'));
 });
