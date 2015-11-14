624c2fa2484c943fc5126caefaed113c2cb58780
Phil Lennon <phil@iampjl.co.uk>
2015-11-14 19:20:07 +0000

    rebuild to use extend type partials in content

--- tasks/metalsmith.js
+++ tasks/metalsmith.js
@@ -7,20 +7,21 @@ var gulpif    = require('gulp-if');
 
 // Specific task modules
 var metalsmith  = require('metalsmith');
-var handlebars = require('handlebars');
-var layouts = require('handlebars-layouts');
+var rename      = require('metalsmith-rename');
+var handlebars  = require('handlebars');
+var layouts     = require('handlebars-layouts');
 
+// Register handlebars layout
 handlebars.registerHelper(layouts(handlebars));
 
 // Build Metalsmith
 function runMetalsmith(callback) {
   // Metalsmith instance and options
   var metal = new metalsmith('.');
-
   metal.source(config.paths.pages);
   metal.destination(config.paths.build);
 
-  // Get metalsmith options
+  // Get metalsmith options from config
   var plugins = config.metalsmith.plugins || {};
 
   // Require plugins and options
@@ -31,10 +32,12 @@ function runMetalsmith(callback) {
     metal.use(plugin(options));
   });
 
-  // Build the site
+  // Rename file extensions
+  metal.use(rename([[/\.hbs$/, '.html'], [/\.md$/, '.html']]));
+
+  // Build Metalsmith or error out
   metal.build(function(err) {
     if (err) {
-      console.log(err);
       return callback(err);
     }
 
