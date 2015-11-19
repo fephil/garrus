6ffefed836a278e0aa794872d26133ef46f5ff5b
Phil Lennon <phil@iampjl.co.uk>
2015-11-19 19:13:44 +0000

    js minification on production flag

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,23 @@
 var webpack = require('webpack');
+var argv    = require('yargs').argv;
+var plugins = [
+  new webpack.ProvidePlugin({
+     $: "jquery",
+     jQuery: "jquery"
+  })
+];
+
+// Add Uglify task if there is a production flag
+if (argv.production) {
+  plugins.push(new webpack.optimize.UglifyJsPlugin());
+}
 
 module.exports = {
   entry: ['./src/js/global.js', './src/js/app.js'],
   output: {
     filename: './_dist/assets/js/bundle.js',
   },
-  plugins: [
-    new webpack.ProvidePlugin({
-       $: "jquery",
-       jQuery: "jquery"
-    })
-  ],
+  plugins: plugins,
   module: {
     preLoaders: [
       {
