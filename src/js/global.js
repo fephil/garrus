b758ae4ed8da179cd9215cf8e07de033013fa835
Phil Lennon <phil@iampjl.co.uk>
2015-12-01 18:36:50 +0000

    add holder.js and fastclick

--- src/js/global.js
+++ src/js/global.js
@@ -1,2 +1,8 @@
+// NPM require
+var Holder = require('holderjs');
+var FastClick = require('fastclick');
+
+FastClick.attach(document.body);
+
 console.log('testing if jquery is loaded globally:');
 console.log($);
