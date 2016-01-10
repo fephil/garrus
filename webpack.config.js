1654b5c69fe913722eff81701ea71b19f0ad357d
Phil Lennon <phil@iampjl.co.uk>
2016-01-10 15:46:27 +0000

    convert to standardjs

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,23 @@
-var argv    = require('yargs').argv;
-var webpack = require('webpack');
+var argv = require('yargs').argv
+var webpack = require('webpack')
 
 // Create plugins array
 var plugins = [
   new webpack.ProvidePlugin({
-     $: "jquery",
-     jQuery: "jquery"
+    $: 'jquery',
+    jQuery: 'jquery'
   })
-];
+]
 
 // Add Uglify task to plugins array if there is a production flag
 if (argv.production) {
-  plugins.push(new webpack.optimize.UglifyJsPlugin());
+  plugins.push(new webpack.optimize.UglifyJsPlugin())
 }
 
 module.exports = {
   entry: ['./src/js/app.js'],
   output: {
-    filename: './_dist/assets/js/bundle.js',
+    filename: './_dist/assets/js/bundle.js'
   },
   plugins: plugins,
   module: {
@@ -27,9 +27,9 @@ module.exports = {
         loader: 'babel',
         exclude: /(node_modules|bower_components)/
       }
-    ],
+    ]
   },
   standard: {
     parser: 'babel-eslint'
   }
-};
+}
